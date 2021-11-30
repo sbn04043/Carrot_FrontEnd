eaa5c4f67da35dac6e3c11e8a58e77c8dfa8ffc7
sbn04043 <sbn04043@gmail.com>
2021-11-30 21:56:16 +0900

    ToolBar 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "./Button";
-import Text from "./Text";
-import Adult from "./Adult";
-import Title from "./Title";
-import Photo from "./Photo";
-import { KeyboardEvent } from "react";
-import Counter from "./Counter";
-import Adder from "./Adder";
-import WelcomeName from "./WelcomeName";
-import ConditionalText from "./ConditionalText";
-import HiddenName from "./HiddenName";
-import { useState, ChangeEvent } from "react";
-import Calculator from "./pages/Test/components/Calculator";
-import Test from "./pages/Test/index";
-
-const App = () => {
-  return <Test />;
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+const App = (): JSX.Element => {
+  return (
+    <main>
+      <AppBar position="fixed">
+        <Toolbar></Toolbar>
+      </AppBar>
+      <Toolbar />
+      내용
+      <AppBar position="fixed" sx={{ top: "auto", bottom: 0 }}>
+        <Toolbar>
+          <Grid container>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                홈
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                동네생활
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                내 근처
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                채팅
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                나와 당근
+              </Box>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </main>
+  );
 };
 
 export default App;
--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,7 +1,35 @@
 import Calculator from "./components/Calculator";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import AppBar from "@mui/material/AppBar";
 
-const Test = () => {
-  return <Calculator />;
+const Test = (): JSX.Element => {
+  return (
+    <Container style={{ backgroundColor: "#022ff" }}>
+      {/* <Grid container style={{ backgroundColor: "#ff2fff" }}>
+        <Grid item xs={12} md={6}>
+          xs-12
+        </Grid>
+        <Grid item xs={12} md={6}>
+          xs-12
+        </Grid>
+      </Grid>
+      <Grid container style={{ backgroundColor: "#ff9fff" }}>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+      </Grid> */}
+    </Container>
+  );
 };
 
 export default Test;
